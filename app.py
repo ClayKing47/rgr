226fcff8ab10f18143204f34c12769af74eb105b
Farid Azizov <azfar98@yandex.ru>
2024-03-26 19:15:39 +0300

    delete all lines

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-from tkinter import *
-from turtle import bgcolor 
-
-def window():
-    root = Tk()
-    root.title("Система для учета грузов")
-    root.geometry('800x600')
-    browse_btn=Button(root, width=20, height=4, bg='#ff0000', text="Войти")
-    browse_btn.grid(column=2, row=1, padx=50)
-    root.mainloop()
-
-window()
+import flet as ft
